65c3de5e87ab1d650bef6d180affdac0c9c3473f
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-12-04 15:28:20 -0500

    search input component

---
+++ src/components/search/search-input.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { inputsData } from "../../constants/inputs-data";
+import "./styles.scss";
+import { fetchATrackStart } from "../../redux/tracks/tracks.actions";
+
+const SearchInput = ({ fetchATrackStart }) => {
+  const [value, setValue] = useState("");
+  const onInputChange = (e) => {
+    // check preventDefault
+    if (e.keyPress === "13") {
+      e.preventDefault();
+    }
+    setValue(e.target.value);
+  };
+
+  const onSearch = (e) => {
+    e.preventDefault();
+    console.log(value);
+    fetchATrackStart(value);
+    setValue("");
+  };
+
+  return (
+    <form className="search-form">
+      <input
+        className="search-input"
+        type="text"
+        placeholder={inputsData.search.placeholder}
+        value={value}
+        onChange={onInputChange}
+      />
+      <button type="submit" className="search-button" onClick={onSearch}>
+        {inputsData.search.buttonActionText}
+      </button>
+    </form>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchATrackStart: (trackName) => dispatch(fetchATrackStart(trackName)),
+});
+
+export default connect(null, mapDispatchToProps)(SearchInput);
