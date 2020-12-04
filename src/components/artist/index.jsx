c4bc8da00a8aa17011ba4bd641769f19c13c4a91
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-12-04 15:27:01 -0500

    top-radio component

---
+++ src/components/artist/index.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import ArtistPreview from "./artist-preview";
+import { fetchArtistStart } from "../../redux/tracks/tracks.actions";
+import "./styles.scss";
+
+const ArtistInfo = ({ match, fetchArtistStart, artistInfo }) => {
+  const artist = match.params.id;
+
+  useEffect(() => {
+    fetchArtistStart(artist);
+  }, [artist, fetchArtistStart]);
+
+  console.log(artist);
+  return (
+    <section className="artist-container">
+      {artistInfo ? (
+        <ArtistPreview
+          name={artist}
+          imgUrl={artistInfo.image[2]["#text"]}
+          info={artistInfo.bio.summary}
+          tags={artistInfo.tags.tag}
+        />
+      ) : null}
+    </section>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  artistInfo: state.tracks.artist,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchArtistStart: (artistName) => dispatch(fetchArtistStart(artistName)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArtistInfo);
